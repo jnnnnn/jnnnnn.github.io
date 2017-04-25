492732d47a70a20e0ab1f4f05b9a5bc85377118e
jnnnnn <jnnnnn@gmail.com>
2017-04-25 17:48:10 +1000

    Renders stations

--- melnet/checker.py
+++ melnet/checker.py
@@ -1,1 +1,9 @@
-for os.path.walk('gtfs')
+import os
+
+for args, dirname, files in os.walk('gtfs'):
+    if 'routes.txt' in files:
+        with open(os.path.join(args, 'routes.txt'), 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+            print(args, len(lines))
+            print("".join(lines[:4]))
+            print()
