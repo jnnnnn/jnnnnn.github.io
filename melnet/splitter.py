e6620a3fc39e40637b536ea045513d5b7e2fc67e
Jonathan Newnham <jnewnham@iress.com.au>
2017-04-27 12:08:55 +1000

    filter to remove irrelevant data (browser was running out of memory for buses)

---
+++ melnet/splitter.py
@@ -0,0 +1,40 @@
+import os, csv, datetime
+
+window_start = datetime.datetime(2017, 5, 1, 10, 0)
+window_end = datetime.datetime(2017, 5, 1, 11, 0)
+
+for args, dirname, files in os.walk('gtfs'):    
+	if 'stop_times.txt' in files:
+		print("Processing", args)
+		services_rows = csv.DictReader(open(os.path.join(args, 'calendar.txt'), 'r', encoding='utf-8-sig'))
+		services = { row['service_id'] : row for row in services_rows } # service_id,monday,tuesday,wednesday,thursday,friday,saturday,sunday,start_date,end_date	
+		for service_id in services:
+			service = services[service_id]
+			service['start_date'] = datetime.datetime.strptime(service['start_date'], '%Y%m%d')
+			service['end_date'] = datetime.datetime.strptime(service['end_date'], '%Y%m%d')
+
+		trips_rows = csv.DictReader(open(os.path.join(args, 'trips.txt'), 'r', encoding='utf-8-sig'))
+		trips = { row['trip_id'] : row for row in trips_rows } # route_id,service_id,trip_id,shape_id,trip_headsign,direction_id
+		
+		# trip_id,arrival_time,departure_time,stop_id,stop_sequence,stop_headsign,pickup_type,drop_off_type,shape_dist_traveled
+		f = csv.DictReader(open(os.path.join(args, 'stop_times.txt'), 'r', encoding='utf-8-sig'))
+		fout = csv.DictWriter(
+			open(os.path.join(args, 'stop_times-filtered.txt'), 'w', encoding='utf-8-sig'), 
+			f.fieldnames,
+			quoting=csv.QUOTE_ALL,
+			lineterminator='\n')
+		fout.writeheader()
+		for row in f: 
+			try:
+				trip = trips[row['trip_id']]
+				service = services[trip['service_id']]
+				if window_end < service['start_date'] or window_start > service['end_date']:
+					continue
+				if service[window_start.strftime('%A').lower()] != "1":
+					continue
+				stoptime = datetime.datetime.strptime(row['departure_time'], '%H:%M:%S').time()
+				if stoptime < window_start.time() or stoptime > window_end.time():
+					continue
+				fout.writerow(row)
+			except Exception as e:
+				print(e)
