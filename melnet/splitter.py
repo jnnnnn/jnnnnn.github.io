1b3ebdf59196ce02a926bf99dd96c628071ec45c
Jonathan Newnham <jnewnham@iress.com.au>
2017-05-11 15:35:18 +1000

    compress stop_times-filtered.txt

--- melnet/splitter.py
+++ melnet/splitter.py
@@ -1,4 +1,4 @@
-import os, csv, datetime
+import os, csv, datetime, zlib
 
 window_start = datetime.datetime(2017, 5, 1, 5, 0) # 1 May, 10am
 window_end = datetime.datetime(2017, 5, 1, 12, 0) # 1 May, 11am
@@ -17,37 +17,47 @@ for args, dirname, files in os.walk('gtfs'):
 		trips = { row['trip_id'] : row for row in trips_rows } # route_id,service_id,trip_id,shape_id,trip_headsign,direction_id
 		
 		# trip_id,arrival_time,departure_time,stop_id,stop_sequence,stop_headsign,pickup_type,drop_off_type,shape_dist_traveled
-		f = csv.DictReader(open(os.path.join(args, 'stop_times.txt'), 'r', encoding='utf-8-sig'))
-		fout = csv.DictWriter(
-			open(os.path.join(args, 'stop_times-filtered.txt'), 'w', encoding='utf-8-sig'), 
-			f.fieldnames,
-			quoting=csv.QUOTE_ALL,
-			lineterminator='\n')
-		fout.writeheader()
-		prevstoptime = None
-		prevtrip = None
-		for row in f: 
-			try:
-				trip = trips[row['trip_id']]
-				service = services[trip['service_id']]
-				if window_end < service['start_date'] or window_start > service['end_date']:
-					continue
-				if service[window_start.strftime('%A').lower()] != "1":
-					continue
-				stoptime = datetime.datetime.strptime(row['departure_time'], '%H:%M:%S').time()								
-				if stoptime < window_start.time() or stoptime > window_end.time():
-					continue				
+		with open(os.path.join(args, 'stop_times.txt'), 'r', encoding='utf-8-sig') as f:
+			fread = csv.DictReader(f)
+			with open(os.path.join(args, 'stop_times-filtered.txt'), 'w', encoding='utf-8-sig') as fout:
+				foutwrite = csv.DictWriter(fout, fread.fieldnames, quoting=csv.QUOTE_ALL, lineterminator='\n')
+				foutwrite.writeheader()
+				prevstoptime = None
+				prevtrip = None
+				for row in fread: 
+					try:
+						trip = trips[row['trip_id']]
+						service = services[trip['service_id']]
+						if window_end < service['start_date'] or window_start > service['end_date']:
+							continue
+						if service[window_start.strftime('%A').lower()] != "1":
+							continue
+						stoptime = datetime.datetime.strptime(row['departure_time'], '%H:%M:%S').time()								
+						if stoptime < window_start.time() or stoptime > window_end.time():
+							continue				
 
-				# detect teleporting vehicles and fudge the stop times so they don't jump
-				if stoptime == prevstoptime and row['trip_id'] == prevtrip:
-					#print("teleportation!", args, row)
-					stoptime = stoptime.replace(second=30)
-					row['departure_time'] = stoptime.strftime('%H:%M:%S')
-					#print(row)
-				prevstoptime = stoptime				
-				prevtrip = row['trip_id']
+						# detect teleporting vehicles and fudge the stop times so they don't jump
+						if stoptime == prevstoptime and row['trip_id'] == prevtrip:
+							#print("teleportation!", args, row)
+							stoptime = stoptime.replace(second=30)
+							row['departure_time'] = stoptime.strftime('%H:%M:%S')
+							#print(row)
+						prevstoptime = stoptime				
+						prevtrip = row['trip_id']
 
-				fout.writerow(row)
-			except Exception as e:
-				#print(e)
-				pass
+						foutwrite.writerow(row)
+					except Exception as e:
+						#print(e)
+						pass				
+
+		# compress the output file (not used yet)
+		# os.chdir(r'd:\drop\jnnnnn.github.io\melnet\gtfs-wellington')
+		f = open(os.path.join(args, 'stop_times-filtered.txt'), 'rb')
+		fout = open(os.path.join(args, 'stop_times-filtered.txt.deflate'), 'wb')
+		compressor = zlib.compressobj()
+		for line in f:
+			bits = compressor.compress(line)
+			fout.write(bits)
+		fout.write(compressor.flush())
+		f.close()
+		fout.close()
