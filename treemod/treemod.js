9c91758af514d8c6f7a9deae8c172b0543d92149
Jonathan Newnham <jnewnham@iress.com.au>
2017-09-21 14:32:07 +1000

    Read data correctly

--- treemod/treemod.js
+++ treemod/treemod.js
@@ -3,10 +3,10 @@ var margin = { top: 20, right: 90, bottom: 30, left: 90 },
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-var svgg = d3.select("svg")
+var svgg = d3.select("svg#chart")
     .attr("width", width + margin.right + margin.left)
     .attr("height", height + margin.top + margin.bottom)
-    .append("g")
+    .append("g") // this doesn't seem to work?
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 var duration = 750;
@@ -21,71 +21,59 @@ function load_text_data() {
         .id(d => d.Index)
         .parentId(d => d.Parent)
         (csv_rows);
+    root.x0 = height/2;
+    root.y0 = 0;
     update(root);
 }
 
-function update(source) {
+function update(root) {
+
+    // compute and set height
+    d3.select('svg#chart')
+        .attr('width', 960)
+        .attr('height', 500);
 
     // Assigns the x and y position for the nodes
-    var treeData = d3.tree().size([height, width])(source);
+    var treeData = d3.tree().size([height, width])(root);
 
     // Compute the new tree layout.
-    var nodes = treeData.descendants(),
-        links = treeData.descendants().slice(1);
-
-    // Normalize for fixed-depth.
-    nodes.forEach(function (d) { d.y = d.depth * 180 });
+    var nodes = treeData.descendants();
+    var links = treeData.descendants().slice(1);
 
     // Update the nodes...
-    var node = svgg.selectAll('g.node')
-        .data(nodes);
+    var node = d3.select('svg#chart').selectAll('g.node')
+        .data(nodes, d => d.id);
 
     var nodeEnterList = node.enter();
 
     var nodeEnter = nodeEnterList.append('g')
         .attr('class', 'node')
-        .attr("transform", function (d) {
-            return "translate(" + source.y0 + "," + source.x0 + ")";
-        })
-        .on('click', click);
-
+        .attr("transform", d => "translate(" + root.y0 + "," + root.x0 + ")");
     nodeEnter.append('circle')
         .attr('class', 'node')
         .attr('r', 1e-6)
-        .style("fill", function (d) {
-            return d._children ? "lightsteelblue" : "#fff";
-        });
+        .style("fill", d => d._children ? "lightsteelblue" : "#fff");
 
     nodeEnter.append('text')
         .attr("dy", ".35em")
-        .attr("x", function (d) {
-            return d.children || d._children ? -13 : 13;
-        })
-        .attr("text-anchor", function (d) {
-            return d.children || d._children ? "end" : "start";
-        })
-        .text(function (d) { return d.data.name; });
+        .attr("x", d => d.children || d._children ? -13 : 13)
+        .attr("text-anchor", d => d.children || d._children ? "end" : "start")
+        .text(d => d.data.Description);
 
     var nodeUpdate = nodeEnter.merge(node);
 
     nodeUpdate.transition()
         .duration(duration)
-        .attr("transform", function (d) {
-            return "translate(" + d.y + "," + d.x + ")";
-        });
+        .attr("transform", d=>"translate(" + d.y + "," + d.x + ")");
 
     nodeUpdate.select('circle.node')
         .attr('r', 10)
-        .style("fill", function (d) {
-            return d._children ? "lightsteelblue" : "#fff";
-        })
+        .style("fill", d => d._children ? "lightsteelblue" : "#fff")
         .attr('cursor', 'pointer');
 
     var nodeExit = node.exit().transition()
         .duration(duration)
-        .attr("transform", function (d) {
-            return "translate(" + source.y + "," + source.x + ")";
-        })
+        .attr("transform", d => "translate(" + root.y + "," + root.x + ")")
         .remove();
 
     nodeExit.select('circle')
@@ -96,24 +84,24 @@ function update(source) {
 
     // Update the links...
     var link = svgg.selectAll('path.link')
-        .data(links, function (d) { return d.id; });
+        .data(links, d => d.id);
 
     var linkEnter = link.enter().insert('path', "g")
         .attr("class", "link")
         .attr('d', function (d) {
-            var o = { x: source.x0, y: source.y0 }
+            var o = { x: root.x0, y: root.y0 }
             return diagonal(o, o)
         });
 
     var linkUpdate = linkEnter.merge(link);
     linkUpdate.transition()
         .duration(duration)
-        .attr('d', function (d) { return diagonal(d, d.parent) });
+        .attr('d', d => diagonal(d, d.parent) );
 
     var linkExit = link.exit().transition()
         .duration(duration)
         .attr('d', function (d) {
-            var o = { x: source.x, y: source.y }
+            var o = { x: root.x, y: root.y }
             return diagonal(o, o)
         })
         .remove();
@@ -130,16 +118,5 @@ function update(source) {
           C ${(s.y + d.y) / 2} ${s.x},
             ${(s.y + d.y) / 2} ${d.x},
             ${d.y} ${d.x}`
-    }
-
-    function click(d) {
-        if (d.children) {
-            d._children = d.children;
-            d.children = null;
-        } else {
-            d.children = d._children;
-            d._children = null;
-        }
-        update(d);
     }
 }
