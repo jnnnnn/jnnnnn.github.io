9adb89b407316f000828459a917d2113469aa51d
jnnnnn <jnnnnn@gmail.com>
2020-02-02 14:19:43 +1100

    create node at mousecoord

--- bookgrapher/modules/commands.js
+++ bookgrapher/modules/commands.js
@@ -55,6 +55,8 @@ const unlink = state => (source, target) => {
 const edit = state => (source, target) => {
   // we might create it here but it is not added to the model until finishEditing
   const textarea = document.createElement("textarea");
+  // save mouse coords for create later
+  const coords = state.mouse;
   textarea.className = "centered";
   textarea.value = target ? target.text : "";
   document.body.append(textarea);
@@ -65,7 +67,7 @@ const edit = state => (source, target) => {
 
   const finishEditing = () => {
     if (target) mutateNode(state)(target, { text: textarea.value });
-    else addNode(state)({ text: textarea.value }, source);
+    else addNode(state)({ text: textarea.value, ...coords }, source);
     textarea.remove();
   };
   textarea.onblur = finishEditing;
