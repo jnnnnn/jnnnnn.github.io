b8694ad7c9a3eef71ded248a18f5cf4cf97ec85f
Jonathan Newnham <jnnnnn@gmail.com>
2022-02-09 16:26:09 +1100

    grid instead of table; fix state management

--- sudoku/index.js
+++ sudoku/index.js
@@ -1,35 +1,40 @@
 import { html, render } from "https://unpkg.com/htm/preact/index.mjs?module";
 import { useState } from "https://unpkg.com/preact@latest/hooks/dist/hooks.module.js?module";
 
-const defaultboard = [...Array(9)].map(() => [...Array(9)].map(() => 1 << 11));
+const flag = 1 << 11;
+
+const defaultboard = [...Array(9)].map(() => [...Array(9)].map(() => flag));
 const App = (props) => {
   const [board, setBoard] = useState(defaultboard);
   const setCell = (row, col) => (e) => {
-    const newBoard = [...board];
-    newBoard[row][col] = e.target.value;
+    const newBoard = copy(board);
+    const number = parseInt(e.target.value);
+    newBoard[row][col] = number > 0 && number < 10 ? number : flag;
     setBoard(newBoard);
     console.log(newBoard);
   };
 
-  return html`<${Board} ...${{ board, setCell }} />`;
+  return html`<${Board} ...${{ board, setCell }} />
+    <button onClick=${() => setBoard(defaultboard)}>Reset</button>
+    <button onClick=${() => setBoard(solve(board))}>Solve</button> `;
 };
 
 const Board = ({ board, setCell }) => {
-  return html`<table>
-    ${board.map(
-      (row, rowindex) => html`<tr>
-        ${row.map(
-          (cell, colindex) => html`<td>
-            <input
-              type="text"
-              value=${cell}
-              onInput=${setCell(rowindex, colindex)}
-            />
-          </td>`
-        )}
-      </tr>`
+  return html`<div class="board">
+    ${board.map((row, rowindex) =>
+      row.map(
+        (cell, colindex) => html`<div class="cell">
+          <input
+            type="text"
+            value=${cell < 10 ? cell : ""}
+            onInput=${setCell(rowindex, colindex)}
+          />
+        </div>`
+      )
     )}
-  </table>`;
+  </div>`;
 };
+
+const copy = (board) => board.map((row) => [...row]);
 
 render(html`<${App} />`, document.body);
