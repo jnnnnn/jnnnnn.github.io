00ebe523266c94d9777fb06bcfd75f0c41c29f3b
jnnnnn <jnnnnn@gmail.com>
2022-08-31 08:27:05 +1200

    buttons for quick entry on mobile

--- sudoku/index.js
+++ sudoku/index.js
@@ -8,57 +8,88 @@ import { trySolve, solvedCells, copy } from "./solver.js";
 const UNSOLVED = 0x1ff << 1;
 const defaultboard = [...Array(9 * 9)].map(() => UNSOLVED);
 const App = () => {
-  const [board, setBoard] = useState(defaultboard);
-  const setCell = (cellIndex) => (e) => {
-    const newBoard = copy(board);
-    const number = parseInt(e.target.value);
-    if (number > 0 && number < 10) {
-      newBoard[cellIndex] = 1 << number;
-    } else {
-      newBoard[cellIndex] = UNSOLVED;
-    }
-    setBoard(newBoard);
-  };
+    const [board, setBoard] = useState(defaultboard);
+    const [selectedNumber, setSelectNumber] = useState(0);
+    const setCell = (cellIndex) => (e) => {
+        const newBoard = copy(board);
+        const number = parseInt(e.target.value);
+        if (number > 0 && number < 10) {
+            newBoard[cellIndex] = 1 << number;
+        } else {
+            newBoard[cellIndex] = UNSOLVED;
+        }
+        setBoard(newBoard);
+    };
 
-  return html`<div class="app">
-    <${Board} ...${{ board, setCell }} />
-    <button onClick=${() => setBoard(defaultboard)}>Reset</button>
-    <button id="solve" onClick=${() => setBoard(trySolve(board))}>Solve</button>
-    <button onClick=${() => setSavedBoard(board)}>Save</button>
-    <button onClick=${() => setBoard(getSavedBoard())}>Load</button>
-  </div>`;
+    return html`<div class="app">
+        <${Board} ...${{ board, setBoard, setCell, selectedNumber }} />
+        <div class="buttons">
+            ${[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(
+                (number) =>
+                    html`<${NumberButton} ...${{ number, setSelectNumber }} />`
+            )}
+        </div>
+        <button onClick=${() => setBoard(defaultboard)}>Reset</button>
+        <button id="solve" onClick=${() => setBoard(trySolve(board))}>
+            Solve
+        </button>
+        <button onClick=${() => setSavedBoard(board)}>Save</button>
+        <button onClick=${() => setBoard(getSavedBoard())}>Load</button>
+    </div>`;
 };
 
-const Board = ({ board, setCell }) => {
-  return html`<div class="board">
-    ${board.map(
-      (cell, cellIndex) => html`<div class="cell">
-        <input
-          type="text"
-          value=${cellToText(cell)}
-          onInput=${setCell(cellIndex)}
-        />
-      </div>`
-    )}
-  </div>`;
+const Cell =
+    ({ board, setBoard, setCell, selectedNumber }) =>
+    (cell, cellIndex) =>
+        html`<div class="cell">
+            <input
+                type="text"
+                value=${cellToText(cell)}
+                onInput=${setCell(cellIndex)}
+                onClick=${CellClick({
+                    board,
+                    setBoard,
+                    cellIndex,
+                    selectedNumber,
+                })}
+            />
+        </div>`;
+
+const CellClick =
+    ({ board, setBoard, cellIndex, selectedNumber }) =>
+    (e) => {
+        e.preventDefault();
+        const newBoard = copy(board);
+        newBoard[cellIndex] = selectedNumber ? 1 << selectedNumber : UNSOLVED;
+        setBoard(newBoard);
+    };
+
+const Board = ({ board, setBoard, setCell, selectedNumber }) => {
+    return html`<div class="board">
+        ${board.map(Cell({ board, setBoard, setCell, selectedNumber }))}
+    </div>`;
 };
+
+// When this button is pressed, it sets the selected number
+const NumberButton = ({ number, setSelectNumber }) =>
+    html`<button onClick=${() => setSelectNumber(number)}>${number}</button>`;
 
 const cellToText = (cell) => {
-  if (cell === UNSOLVED) return "";
-  if (solvedCells.has(cell)) return solvedCells.get(cell);
-  if (cell === 0) return "X";
-  return "?";
+    if (cell === UNSOLVED) return "";
+    if (solvedCells.has(cell)) return solvedCells.get(cell);
+    if (cell === 0) return "X";
+    return "?";
 };
 
 // save the board to local storage
 const setSavedBoard = (board) => {
-  localStorage.setItem("sudoku", JSON.stringify(board));
+    localStorage.setItem("sudoku", JSON.stringify(board));
 };
 
 // retrieve the saved board from local storage
 const getSavedBoard = () => {
-  const savedBoard = localStorage.getItem("sudoku");
-  return savedBoard ? JSON.parse(savedBoard) : defaultboard;
+    const savedBoard = localStorage.getItem("sudoku");
+    return savedBoard ? JSON.parse(savedBoard) : defaultboard;
 };
 
 render(html`<${App} />`, document.body);
