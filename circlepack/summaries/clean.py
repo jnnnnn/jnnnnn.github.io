bb4f3d0c01d77654ff93bca1aac30ed2324f5e29
jnnnnn <jnnnnn@gmail.com>
2023-03-09 17:35:33 +1300

    cleaned cards so far

--- circlepack/summaries/clean.py
+++ circlepack/summaries/clean.py
@@ -3,15 +3,17 @@
 import bs4
 import glob
 
+
 def clean_summary(soup):
     # remove styles and IDs from all tags
     for tag in soup.find_all(True):
-        del tag['style']
-        del tag['id']
-    
-for filename in glob.glob('*.html'):
-    with open(filename, 'r') as f:
-        soup = bs4.BeautifulSoup(f, 'html.parser')
+        del tag["style"]
+        del tag["id"]
+
+
+for filename in glob.glob("*.html"):
+    with open(filename, "r", encoding="utf-8", errors="ignore") as f:
+        soup = bs4.BeautifulSoup(f, "html.parser")
     clean_summary(soup)
-    with open(filename, 'w') as f:
+    with open(filename, "w", encoding="utf-8", errors="ignore") as f:
         f.write(str(soup))
